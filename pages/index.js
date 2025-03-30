027d183b58f7594a6c0f2dfc619a3489a396d2ce
Germano Bender <germanotech21@gmail.com>
2025-03-29 22:51:56 -0300

    melhoria hehe

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Tenho muito orgulho de você meu amorzão ❤️ Você que me motiva todos os dias</h1>;
+    return <h1>Franciele ❤️ Tenho muito orgulho de você meu amorzão ❤️ Você que me motiva todos os dias❤️</h1>;
 }
 
 export default Home;
