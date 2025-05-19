1f4b7b68e6514f4c28f2249515b8bfddc2fc7da0
Germano Bender <germanotech21@gmail.com>
2025-05-18 21:35:27 -0300

    configuração prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Franciele ❤️ Tenho muito orgulho de você meu amorzão ❤️ Você que me motiva todos os dias❤️</h1>;
+  return (
+    <h1>
+      Franciele ❤️ Tenho muito orgulho de você meu amorzão ❤️ Você que me motiva
+      todos os dias❤️
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
